3e8768c97a62c956cd81f95a2822d945ec512c16
Lynette Li <huan.li@cix.csi.cuny.edu>
2022-01-07 00:11:14 -0500

    assignment4

--- scripts.js
+++ scripts.js
@@ -1,4 +1,22 @@
 
+$(document).ready(function() {
+
+	function printHTML(text){
+		let printHTML = document.getElementById('text');
+		printHTML.innerHTML += text;
+	}
+
+
+	$('#buttonOne').click(function(){
+        $('.text2').remove();
+			printHTML("<h1 class='text1'>I'm right! </h1>" );
+		})
+
+	$('#buttonTwo').click(function(){
+        $('.text1').remove();
+			printHTML("<h1 class='text2'> No, I'm right!! </h1>");
+    })
+})
 
 const calculateVolume = () => {
   console.log("Button clicked!")
@@ -15,3 +33,7 @@ const calculateVolume = () => {
 
   return false;
 }
+
+$('.noHover').hover(function(){
+  alert("Hey, I told you to not hover over me!");
+})
